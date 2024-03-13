b2ee3461822549a1688902d301993c42fc222a81
John Meyer <meyerjohn44@gmail.com>
2024-03-12 20:20:32 -0400

    Functioning Navbar Commit

--- weddding_website/src/components/Navbar.js
+++ weddding_website/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from "../assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
@@ -11,11 +11,10 @@ import {
     ListItemText 
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/info";
+import InfoIcon from "@mui/icons-material/Info";
 import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "mui/icons-material/ShoppingCartrounded";
-import { ShoppingCartRounded } from '@mui/icons-material';
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
 
 
 const Navbar = () => {
@@ -45,7 +44,26 @@ const Navbar = () => {
         
     ]
   
-    return <div className="home-container"><Navbar /></div>
-}
+    return (
+    <nav>
+        <div className="nav-logo-container">
+            <img src={Logo} alt=""/>
+        </div>
+        <div className="navbar-links-container">
+            <a href = "">Home</a>
+            <a href = "">About</a>
+            <a href = "">Testimonials</a>
+            <a href = "">Contact</a>
+            <a href = "">
+                <BsCart2 className="navbar-cart-icon"/>
+            </a>
+            <button className ="primary-button">Bookings Now</button>
+        </div>
+        <div className="navbar-menu-container">
+            <HiOutlineBars3 onClick={() => setOpenMenu(true)}/>
+        </div>
+    </nav>
+    );
+};
 
 export default Navbar
