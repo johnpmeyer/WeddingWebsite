853163e0317d4f60db52107410d590192539de65
John Meyer <meyerjohn44@gmail.com>
2024-03-12 20:38:00 -0400

    Added homepage image, minor edits to folder names

--- weddding_website/src/components/Home.js
+++ weddding_website/src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Navbar from './Navbar';
+import BannerBackground from "../Assets/home-banner-background.png"
+import BannerImage from "../Assets/home-banner-image.png"
+import { FiArrowRight } from "react-icons/fi"
 
 const Home = () => {
   return (
     <div className="home-container"> 
       <Navbar/>
+      <div className = "home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBackground} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
