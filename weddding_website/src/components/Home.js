e79ab9f9d81936df9d9e111a91a2e51b58f86bdb
John Meyer <meyerjohn44@gmail.com>
2024-03-15 14:36:55 -0400

    Finished up home component image and text

--- weddding_website/src/components/Home.js
+++ weddding_website/src/components/Home.js
@@ -12,6 +12,20 @@ const Home = () => {
         <div className="home-bannerImage-container">
           <img src={BannerBackground} alt="" />
         </div>
+      <div className="home-text-section">
+        <h1 className="primary-heading">
+          Your Favorite Food Delivered Hot  & Fresh
+        </h1>
+        <p className="primary-text">
+          Healthy switcher chefs do all the prep work like peeling, chopping, and marinating so you can cook fresh food.
+        </p>
+        <button className="secondary-button">
+          Order Now <FiArrowRight/>
+        </button>
+      </div>
+      <div className="home-image-container">
+        <img src={BannerImage} alt=""></img>
+      </div>
       </div>
     </div>
   )
