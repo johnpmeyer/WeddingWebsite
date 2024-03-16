319e36d146973d03c580d0c085ee8a3c7eafa30b
John Meyer <meyerjohn44@gmail.com>
2024-03-16 10:12:47 -0400

    Working on navbar and initial styling

--- weddding_website/src/App.js
+++ weddding_website/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar.js';
 
 function App() {
+
   return (
-    <div className="App">
-     Welcome to my website
-    </div>
+    <Navbar/>
   );
 }
 
--- weddding_website/src/components/Navbar.js
+++ weddding_website/src/components/Navbar.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import Logo from "../Assets/logo-joke.jpg";
+
 
 function Navbar() {
+
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home"
+    }, 
+    {
+      text: "Event Details"
+    }, 
+    {
+      text: "Things to Do / Where to Stay"
+    }, 
+    {
+      text: "Puzzles and Fun ;)"
+    }, 
+    {
+      text: "Comments"
+    }
+  ]
+
   return (
-    <>
-      <nav className = "navbar">
-        <div className = "navbar-container">
-            <Link to = "/" className = "navbar-logo">
-                Home
-            </Link>
-        </div>
-      </nav>
-    </>
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="Bon Jovi Have a Nice Day Logo" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">Event Details</a>
+        <a href="">Things to Do / Where to Stay</a>
+        <a href="">Puzzles and Fun ;)</a>
+        <a href="">Comments</a>
+      </div>
+    </nav>
   )
 }
 
