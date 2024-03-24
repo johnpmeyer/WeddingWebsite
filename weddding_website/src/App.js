e5d2bf47f119f07ba7f50c0cd12b32bd2ebe05b1
John Meyer <meyerjohn44@gmail.com>
2024-03-24 10:30:20 -0400

    adding event details

--- weddding_website/src/App.js
+++ weddding_website/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from './Components/Home.js';
+import EventDetails from './Components/Event_Details.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-        <Route exact path="/event-details" element={<Home />}></Route>
+        <Route exact path="/event-details" element={<EventDetails />}></Route>
         <Route exact path="/things-to-do" element={<Home />}></Route>
         <Route exact path="/puzzles-fun" element={<Home />}></Route>
         <Route exact path="/comments" element={<Home />}></Route>
---
+++ weddding_website/src/components/Event_Details.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Event_Details() {
+  return (
+    <div className='event-details-container'>
+        <Navbar/>
+        <div className='event-details-main-text-container'>
+            <h1 className='primary-heading'>Event Details</h1>
+            <p className='primary-text'><strong>Location:</strong> WBU Event Venue in Troy Hill, Pittsburgh, PA.</p>
+            <p className='primary-text'>Other info coming in late 2024</p>
+        </div>
+    </div>
+  )
+}
+
+export default Event_Details
