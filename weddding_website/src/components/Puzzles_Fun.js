7dbbc2e49f995baa9185dae37932827256969a71
John Meyer <meyerjohn44@gmail.com>
2024-10-16 19:54:34 -0400

    including simple form and update to comments and css

--- weddding_website/src/components/Puzzles_Fun.js
+++ weddding_website/src/components/Puzzles_Fun.js
@@ -1,20 +1,125 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from './Navbar'
 import ConstructionImage from "../Assets/under_construction.jpg"
 
-function Puzzles_Fun() {
-    return (
-      <div className='App'>
-        <div className='event-details-container'>
-            <Navbar/>
-            <div className='event-details-main-text-container'>
-              <p className='primary-text'>Coming Some Day</p>
-              <img src={ConstructionImage} alt=""></img>
-            </div>
-        </div>
-      </div>
+const QuizForm = () => {
+  // Initial state for storing answers (optional, if you want to capture them later)
+  const [answers, setAnswers] = useState({
+    question1: "",
+    question2: "",
+    question3: "",
+  });
 
-    )
-  }
-  
-  export default Puzzles_Fun
+  // Handle input changes (this can be used later if you decide to store answers)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAnswers({
+      ...answers,
+      [name]: value,
+    });
+  };
+
+  return (
+      <div className='App'>
+        <div className='puzles-games-container'>
+        <Navbar/>
+          <div className = 'puzzles-games-main-text-container'>
+            <h2 className='primary-heading'>Quiz!</h2>
+            <form className='puzzles-games-forms'>
+              {/* Question 1 - Short answer */}
+              <div>
+                <label htmlFor="question1">1. What is the capital of France?  </label>
+                <input
+                  type="text"
+                  id="question1"
+                  name="question1"
+                  value={answers.question1}
+                  onChange={handleChange} // This isn't necessary yet but can be used later
+                  placeholder="Enter your answer"
+                />
+              </div>
+
+              {/* Question 2 - Multiple choice */}
+              <div>
+                <label>2. Which of these is a programming language?</label>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="radio"
+                    id="python"
+                    name="question2"
+                    value="Python"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="python">Python</label>
+                </div>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="radio"
+                    id="html"
+                    name="question2"
+                    value="HTML"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="html">HTML</label>
+                </div>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="radio"
+                    id="css"
+                    name="question2"
+                    value="CSS"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="css">CSS</label>
+                </div>
+              </div>
+
+              {/* Question 3 - Checkbox */}
+              <div>
+                <label>3. Select the planets in our solar system:</label>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="checkbox"
+                    id="earth"
+                    name="question3"
+                    value="Earth"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="earth">Earth</label>
+                </div>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="checkbox"
+                    id="mars"
+                    name="question3"
+                    value="Mars"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="mars">Mars</label>
+                </div>
+                <div className = 'puzzles-games-input'>
+                  <input
+                    type="checkbox"
+                    id="jupiter"
+                    name="question3"
+                    value="Jupiter"
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="jupiter">Jupiter</label>
+                </div>
+              </div>
+
+              {/* Submit button */}
+              <div>
+                <button type="submit" className='primary-text' disabled>
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default QuizForm;
